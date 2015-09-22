dcbea5217c585c8d10b221d36e4d2515c8b401de
Theresa Palid <tpalid1@gmail.com>
2015-09-22 18:52:48 +0000

    Updated gemfile for deployment to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  YourAppName::Application.routes.draw do
+    root "application#hello"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
